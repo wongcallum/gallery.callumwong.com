49ea3423bd4720b43215efacf7d0d1ed422b6453
callum <ccallum.wong@gmail.com>
2025-06-09 22:46:40 +1000

    ok next

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -3,9 +3,11 @@ import { Card, CardContent } from "~/components/ui/card";
 import { signIn } from "~/server/auth";
 import { providerMap } from "~/server/auth/config";
 
-export default function LoginPage(props: {
-	searchParams: { callbackUrl: string | undefined };
+export default async function LoginPage(props: {
+	searchParams: Promise<{ callbackUrl: string | undefined }>;
 }) {
+	const params = await props.searchParams;
+
 	return (
 		<div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
 			<div className="w-full max-w-sm">
@@ -17,9 +19,8 @@ export default function LoginPage(props: {
 									key={provider.id}
 									action={async () => {
 										"use server";
-										const params = await props.searchParams;
 										await signIn(provider.id, {
-											redirectTo: (await params.callbackUrl) ?? "",
+											redirectTo: params.callbackUrl ?? "",
 										});
 									}}
 								>

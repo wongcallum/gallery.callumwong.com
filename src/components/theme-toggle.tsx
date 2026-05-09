7649b83120b94ac36781b7a889399367aab9ffe7
Callum <mail@callumwong.com>
2026-05-09 13:05:51 +1000

    fix theem toggle hydration error

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -2,10 +2,18 @@
 
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { Button } from "~/components/ui/button";
 
 export function ThemeToggle() {
 	const { resolvedTheme, setTheme } = useTheme();
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => setMounted(true), []);
+
+	if (!mounted) {
+		return <Button variant="ghost" size="icon" className="size-4" />;
+	}
 
 	return (
 		<Button
